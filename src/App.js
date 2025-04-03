dcab136ab3e8418511b30d198377ccef525d555c
Atharvaa20 <guravatharv2003@gmail.com>
2025-04-04 01:00:54 +0530

    Testing CI/CD pipeline update

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-           Deployed via CI/CD Pipeline! 
-        </p>
+        <p>CI/CD Pipeline Test - Auto Deployment</p>
         <a
           className="App-link"
           href="https://reactjs.org"
