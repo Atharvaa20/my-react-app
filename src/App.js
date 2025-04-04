4e1a7f66b5be3b9da84a3f9860554141b59c3dc0
Atharvaa20 <guravatharv2003@gmail.com>
2025-04-04 13:11:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>CI/CD Working!</p>
+        <p>CI/CD is Working good!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
